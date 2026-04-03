a6df61c44c1afa6e80308fd58bf36a857e1837d1
Dmitrij Rebrov <dmitrij.rebrov@inbox.lt>
2026-04-03 17:42:47 +0300

    new mockapi structure and data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import helloKittyImage from "../assets/avatars/icons8-hello-kitty-80.png";
+import marioImage from "../assets/avatars/icons8-super-mario-64.png";
+import homerImage from "../assets/avatars/icons8-homer-simpson-80.png";
 
 function Home() {
     const [textData, setTextData] = useState([]);
@@ -9,7 +12,7 @@ function Home() {
         const loadTextData = async () => {
             try {
                 const response = await fetch(
-                    "https://69cd04efddc3cabb7bd1ee9e.mockapi.io/api/v1/Text"
+                    "https://69cd04efddc3cabb7bd1ee9e.mockapi.io/api/v1/Home"
                 );
 
                 if (!response.ok) {
@@ -28,32 +31,33 @@ function Home() {
         loadTextData();
     }, []);
     useEffect(() => {
-        console.log("textData changed:", textData[0]?.Catchphrase);
+        console.log("textData changed:",  textData);
     }, [textData]);
   return (
     <div>
+
       <section className="relative min-h-[870px] flex items-center overflow-hidden bg-surface-container-low">
           <div className="max-w-7xl mx-auto px-6 grid md:grid-cols-12 gap-12 items-center relative z-10">
               <div className="md:col-span-7 space-y-8">
                   <div className="inline-flex items-center gap-2 px-3 py-1 rounded-full bg-secondary-container text-on-secondary-container text-xs font-label uppercase tracking-widest font-semibold">
                       <span className="w-2 h-2 rounded-full bg-tertiary-fixed-dim"></span>
-                      Precision Engineering
+                      { textData[0]?.hero[0].badge}
                   </div>
                   <h1 className="font-headline text-5xl md:text-7xl font-extrabold tracking-tighter text-primary leading-[1.1]">
-                      Architectural <br/>
-                      <span className="text-primary-container opacity-90">IT Solutions</span> <br/>
-                      for Growth
+                      { textData[0]?.hero[0].title[0]} <br/>
+                      <span className="text-primary-container opacity-90">{ textData[0]?.hero[0].title[1]}</span> <br/>
+                      { textData[0]?.hero[0].title[2]}
                   </h1>
                   <p className="body-lg text-on-surface-variant max-w-xl leading-relaxed">
-                      We don't just fix systems; we design digital foundations. Our consultants build scalable, secure infrastructure tailored to the architectural integrity of your business goals.
+                      { textData[0]?.hero[0].description}
                   </p>
                   <div className="flex flex-wrap gap-4 pt-4">
                       <button className="px-8 py-4 bg-primary-container text-on-primary rounded-md font-headline font-bold flex items-center gap-2 shadow-xl shadow-primary-container/20">
-                          Partner With Us
-                          <span className="material-symbols-outlined">arrow_forward</span>
+                          { textData[0]?.hero[0].buttons[0].label}
+                          <span className="material-symbols-outlined">→</span>
                       </button>
                       <button className="px-8 py-4 bg-surface-container-high text-primary-container rounded-md font-headline font-bold">
-                          Our Methodology
+                          { textData[0]?.hero[0].buttons[1].label}
                       </button>
                   </div>
               </div>
@@ -64,8 +68,8 @@ function Home() {
                       <div className="absolute bottom-6 left-6 right-6 p-6 glass-nav bg-white/10 rounded-lg border border-white/20">
                           <div className="flex items-center justify-between">
                               <div>
-                                  <div className="text-white text-2xl font-bold font-headline">99.9%</div>
-                                  <div className="text-white/70 text-xs font-label">System Reliability</div>
+                                  <div className="text-white text-2xl font-bold font-headline">{ textData[0]?.hero[0].stats[0].value}</div>
+                                  <div className="text-white/70 text-xs font-label">{ textData[0]?.hero[0].stats[0].label}</div>
                               </div>
                                   <span className="material-symbols-outlined text-tertiary-fixed-dim" style={{fontVariationSettings: "'FILL' 1"}}>analytics</span>
                           </div>
@@ -111,7 +115,7 @@ function Home() {
                 <div>
                     <span className="material-symbols-outlined text-4xl text-tertiary-fixed-dim mb-8" data-icon="shield">shield</span>
                     <h3 className="font-headline text-2xl font-bold mb-4">Fortified Security</h3>
-                    <p className="text-on-primary-container leading-relaxed">{textData[0]?.Catchphrase || "Loading..."}</p>
+                    <p className="text-on-primary-container leading-relaxed">Some shit</p>
                 </div>
                 <button className="w-full py-4 bg-surface-container-lowest/10 border border-white/20 rounded-md font-headline font-bold text-sm hover:bg-white/10 transition-colors">
                     Security Audit
@@ -169,12 +173,12 @@ function Home() {
                     </div>
                     <p className="italic text-on-surface mb-8 leading-relaxed">"TechArch restructured our entire server cluster. The ROI was visible within the first quarter due to zero downtime during peak loads."</p>
                     <div className="flex items-center gap-4">
-                        <div className="w-12 h-12 rounded-full bg-primary-container overflow-hidden">
-                            <img alt="Marcus Chen portrait" className="w-full h-full object-cover" data-alt="Professional headshot of a male executive" src="https://lh3.googleusercontent.com/aida-public/AB6AXuC1zodW3DNbUcyjaJsuczevYYhrAYGp8K3zcM8XjRSEBQLpdYIYyYspblJo3wAtvEKnWhw0VLwIPLJ762S10qAofZMtPbAiaEcsQsaJMe5u-arxMcO1VpNEP0KTJnh5xhFxhwqUKDm5e4d7Qb6iZ6283bCRxJWkJeMCmJZXwg2883amHJa34EqA109O2xi1A93f6v3KZjFVAJjkXTVcDf6ctRZOfdrKADIybHwwxO1o3QEbKjKhs_miz8sV8_jrWeGtvpIaS2O90Klf"/>
+                        <div className="w-12 h-12 rounded-full bg-blue-100 overflow-hidden">
+                            <img alt="Marcus Chen portrait" className="w-full h-full object-cover" data-alt="Professional headshot of a male executive" src={homerImage}/>
                         </div>
                         <div>
-                            <div className="font-headline font-bold text-sm">Marcus Chen</div>
-                            <div className="text-xs text-on-surface-variant">CTO, Vertex Dynamics</div>
+                            <div className="font-headline font-bold text-sm">Homer Simpson</div>
+                            <div className="text-xs text-on-surface-variant">Safety Inspector, Springfield Nuclear Power Plant </div>
                         </div>
                     </div>
                 </div>
@@ -189,12 +193,12 @@ function Home() {
                     </div>
                     <p className="italic text-on-surface mb-8 leading-relaxed">"Their security audit was terrifyingly thorough. We've never felt more protected. They treat our data as if it were their own."</p>
                     <div className="flex items-center gap-4">
-                        <div className="w-12 h-12 rounded-full bg-primary-container overflow-hidden">
-                            <img alt="Sarah Jenkins portrait" className="w-full h-full object-cover" data-alt="Professional headshot of a female executive" src="https://lh3.googleusercontent.com/aida-public/AB6AXuAgD9bt3BVmJAI8nXEquMz3Pm08bttIrO5EF-tgpGQCsaUWmBZqhvGYJBj9988Egp3hnBGoX77p6HOjgCVZ10e3tvx7XpVYHHNpFKPgz8iixGcx1bnpZYPEKIJmIOFRBjSBEb9Oyxmw3BVMCE-fJWtZOfhqmYqL19DTcdCb2eXkDCSHkwqquJd0_nO7Qw99d51d53nV_q_hVaQ0M_9HSlHgDBGx2otOvxkDWEGoS6NGh2DmUQ-A6QXpZqw-DHJpLEOYB6TITjR-Y1nh"/>
+                        <div className="w-12 h-12 rounded-full bg-blue-100 overflow-hidden">
+                            <img alt="Hello Kitty portrait" className="w-full h-full object-cover" data-alt="Professional headshot of a cat" src={helloKittyImage}/>
                         </div>
                         <div>
-                            <div className="font-headline font-bold text-sm">Sarah Jenkins</div>
-                            <div className="text-xs text-on-surface-variant">Director of IT, Meridian Global</div>
+                            <div className="font-headline font-bold text-sm">Kitty White</div>
+                            <div className="text-xs text-on-surface-variant">Art Director, Sanrio Company, Ltd.</div>
                         </div>
                     </div>
                 </div>
@@ -209,12 +213,12 @@ function Home() {
                     </div>
                     <p className="italic text-on-surface mb-8 leading-relaxed">"Architectural excellence is the only way to describe their networking approach. Truly the digital blueprint specialists."</p>
                     <div className="flex items-center gap-4">
-                        <div className="w-12 h-12 rounded-full bg-primary-container overflow-hidden">
-                            <img alt="David Okoro portrait" className="w-full h-full object-cover" data-alt="Professional headshot of a smiling male professional" src="https://lh3.googleusercontent.com/aida-public/AB6AXuCiJkJDOUEZbKpC2T6a0Ojo1B99asGh8x7qrax75wYcEMWuJDk9n2pKxqvz8pLytgitCGb8LrMIYf2-TM7ls4M4rRwbjFZYxO83gqFVPnR8XeIB4LBd9urQKRzyV1d1zK7CFC-8jxH-MkwNlNUwboYYjYhH9USai3jyImfPWtWpGQt0nGOew3j-Q4V1YKYc-0VKv6FAXZoYK0jm7POtEBUWWhUY8p6YJXZIvObvFJl_Yefa3FfWNZUL28Wwe4C3ZpOKcu8CFhx3xh7e"/>
+                        <div className="w-12 h-12 rounded-full bg-blue-100 overflow-hidden">
+                            <img alt="David Okoro portrait" className="w-full h-full object-cover" data-alt="Professional headshot of a plumber" src={marioImage}/>
                         </div>
                         <div>
-                            <div className="font-headline font-bold text-sm">David Okoro</div>
-                            <div className="text-xs text-on-surface-variant">Infrastructure Lead, NexaGen</div>
+                            <div className="font-headline font-bold text-sm">Mario Mario</div>
+                            <div className="text-xs text-on-surface-variant">Self employed, Plumber</div>
                         </div>
                     </div>
                 </div>
